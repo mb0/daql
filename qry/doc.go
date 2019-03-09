dd3b76b35b2b56b757143e6c0f278ecc6b6b37e0
Martin Schnabel <mb0@mb0.org>
2019-03-09 21:40:45 +0100

    qry: add some planing notes and change some aspects of the proposed syntax

--- qry/doc.go
+++ qry/doc.go
@@ -1,5 +1,25 @@
 /*
-Package qry extends the xelf language with query expressions to retrieve external resources.
+Package qry extends the xelf language with an expressions to query external data sources.
+
+The qry expression either takes plain arguments representing one unnamed task, that returns the
+result as-is. Or declarations, that each represent a named task and whose results are combined into
+an object.
+
+A task can either be a expression that is resolved in the query environment or a query to a data
+source. Tasks have names and can be nested to build a query tree. Expression tasks may again have
+nested query expressions, those nested queries are always added to the outermost query plan.
+
+There are different queries, that result either in a list of elements, a single element or the
+element count. Where element is most often an obj literal or field literal from within an obj.
+
+The query has a reference that identifies the data source. The reference uses a special prefix to
+distinguish query types. The reference than usually continues with an unmarked schema reference, but
+can also be relative path pointing to another task or even a literal outside the qry environment.
+
+Queries have a selection, that defaults to all fields for obj queries or the scalar element for all
+others. This selection can be used to rename, filter out or add additional fields. This can be used
+for computed fields like sub queries and should also be able to return object elements from scalar
+queries.
 
 (qry
 	(() Selects any one record into a result. Result names must be unique within a qry.
@@ -10,7 +30,7 @@ Package qry extends the xelf language with query expressions to retrieve externa
 	+ ?prod.cat.name
 
 	(() Selects can query all records, or use a limit and offset)
-	+all *prod.cat
+	+all   *prod.cat
 	+top10 *prod.cat :lim 10
 	+page3 *prod.cat :off [20 10]
 
@@ -22,15 +42,15 @@ Package qry extends the xelf language with query expressions to retrieve externa
 	+mats #prod.cat (eq .kind 'material')
 
 	(() a field mapping can be used to filter or rename fields)
-	+infoLabel *label.label (:: .id +label ('Label: ' .name))
-	+leanLabel *label.label (:: -tmpl) (() when sel starts with a - decl '.' is implied)
+	+infoLabel *label.label (:: +id +label ('Label: ' .name))
+	+leanLabel *label.label (:: -tmpl (() when sub starts with a - decl '+' is implied))
 
-	(() Select nested queries. The '+.' includes all fields. The '..id' refers to the parent id)
-	+nest ?prod.cat (eq .name $name) (:: .
-		+prods *prod.prod (eq .cat ..id) :ord .name (:: .id .name)
+	(() Select nested queries. The '+' includes all fields. The '..id' refers to the parent id)
+	(+nest ?prod.cat (eq .name $name) :: +
+		+prods *prod.prod (eq .cat ..id) :asc .name (:: +id +name)
 	)
 	(() Previous results can be used in the query)
-	+top10prods *prod.prod (in .cat /top10/id) :ord .name
+	+top10prods *prod.prod (in .cat /top10/id) :asc .name
 )
 */
 package qry
