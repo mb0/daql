93ce38f0ff016721673cab05eb3837581e21717e
Martin Schnabel <mb0@mb0.org>
2019-05-18 02:43:25 +0200

    qry: edit package docs

--- qry/doc.go
+++ qry/doc.go
@@ -1,32 +1,28 @@
 /*
-Package qry extends the xelf language with an expressions to query external data sources.
+Package qry extends the xelf language with a form to construct query functions for external data.
 
-The qry expression either takes plain arguments representing one unnamed task, that returns the
-result as-is. Or declarations, that each represent a named task and whose results are combined into
-an object.
+The qry form constructs a query plan for a configured model project. The form either takes plain
+arguments representing one unnamed task, or declarations, that each represent a named task and whose
+results are combined into a record literal. A query plan, once resolved, can be cached and reused.
 
-A task can either consist of an expression, that is resolved in the query environment, or a query to
-a data source. Tasks have names and can be nested to build a query tree. Expression tasks may again
-have nested query expressions, those nested queries are always added to the outermost query plan.
+The plan's root tasks can either be query or expression tasks. Each query task has a selection, that
+consists of one or more tasks. The query selection tasks can again be expressions or sub queries.
+Selections without an explicit query or expression, act as a reference to the subject field of the
+same name.
 
-There are different queries, that result either in a list of elements, a single element or the
-element count. Where element is most often an obj literal or field literal from within an obj.
-
-The query has a reference that identifies the data source. The reference uses a special prefix to
-distinguish query types. The reference than usually continues with an unmarked schema reference, but
+The query has a reference that identifies the data source. The reference uses special prefixes to
+distinguish query types. The reference then usually continues with an unmarked schema reference, but
 can also be relative path pointing to another task or even a literal outside the qry environment.
+Three different query prefixes indicate, whether the result is a list of elements, a single element
+or the element count.
 
-Queries have a selection, that defaults to all fields for obj queries or the scalar element for all
-others. This selection can be used to rename, filter out or add additional fields. This can be used
-for computed fields like sub queries and should also be able to return object elements from scalar
-queries.
+The following paragraphs are planned and not yet implemented.
 
-The qry expression resolves a plan with one or more tasks, that plan is then executed by a backend.
-The backend implementation is configurable in the query environment and directs the execution
-process. A simple in-memory backend might need to handle filtering, ordering and selection on its
-own, but can evaluate one task after the other. While a specialized sql backend can off-load most of
-the work by batching independent queries and merging nested queries. A caching backend could
-delegate to an in-memory backend for cached data and another sql backend as its data source.
+The plan acts as a function spec that takes one record parameter and returns the plan's result. This
+allows users to use inline query evaluation, when the situation calls for it.
+
+Expression task can reference previous results or contain nested query constructors or executions.
+This would allow conditional queries or post processing of query results.
 
 (qry
 	(() Selects any one record into a result. Result names must be unique within a qry.
@@ -49,12 +45,12 @@ delegate to an in-memory backend for cached data and another sql backend as its 
 	+mats #prod.cat (eq .kind 'material')
 
 	(() a field mapping can be used to filter or rename fields)
-	+infoLabel *label.label (:: +id +label ('Label: ' .name))
-	+leanLabel *label.label (:: -tmpl (() when sub starts with a - decl '+' is implied))
+	(+infoLabel *label.label +id +label ('Label: ' .name))
+	(+leanLabel *label.label -tmpl (() when sub starts with a - decl '+' is implied))
 
 	(() Select nested queries. The '+' includes all fields. The '..id' refers to the parent id)
-	(+nest ?prod.cat (eq .name $name) :: +
-		+prods *prod.prod (eq .cat ..id) :asc .name (:: +id +name)
+	(+nest ?prod.cat (eq .name $name)
+		+ +prods *prod.prod (eq .cat ..id) :asc .name (:: +id +name)
 	)
 	(() Previous results can be used in the query)
 	+top10prods *prod.prod (in .cat /top10/id) :asc .name
