5396f86da4c2592c3f329ceef267c4d83e70bb42
Martin Schnabel <mb0@mb0.org>
2019-09-02 00:00:40 +0200

    hub: services handle returns false not err if no service was found

--- hub/service.go
+++ hub/service.go
@@ -1,7 +1,5 @@
 package hub
 
-import "github.com/mb0/xelf/cor"
-
 // A service is a common interface for the last message processor in line.
 // It usually is used by wrappers, that handles request parsing and delegate.
 type Service interface {
@@ -14,14 +12,14 @@ type Services map[string]Service
 
 // Handle calls the service with m's subject or returns an error.
 // If the service returns data and c is not nil, a reply is sent to the sender.
-func (s Services) Handle(m *Msg, c Conn) error {
+func (s Services) Handle(m *Msg, c Conn) bool {
 	f := s[m.Subj]
 	if f == nil {
-		return cor.Errorf("service not supported %s", m.Subj)
+		return false
 	}
 	res := f.Serve(m)
 	if res != nil && c != nil {
 		m.From.Chan() <- &Msg{From: c, Subj: m.Subj, Data: res}
 	}
-	return nil
+	return true
 }
